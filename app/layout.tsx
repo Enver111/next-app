da1f9b253ab5990444648c55490b0c0908d5cdee
Enver <enver.erfanovich@gmail.com>
2024-08-07 19:50:40 +0300

    Changed the width of the main page

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={nunito.className}>
+        <main className="min-h-screen">{children}</main>
+      </body>
     </html>
   );
 }
