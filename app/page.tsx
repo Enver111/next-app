13e3a5f518e89a6c477f5856bcc19c334a6acab2
Enver <enver.erfanovich@gmail.com>
2024-08-15 19:23:01 +0300

    Mounted top-bar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
-import { Categories, Container, Title } from "@/components/shared";
+import {
+  Categories,
+  Container,
+  SortPopup,
+  Title,
+  TopBar,
+} from "@/components/shared";
 
 export default function Home() {
   return (
     <>
       <Container className="mt-8">
         <Title text="Все запчасти" size="lg" className="font-extrabold" />
-
-        <Categories />
       </Container>
+      <TopBar />
     </>
   );
 }
