b116358cdb5e7a860ee9ad656cd061655639413f
Enver <enver.erfanovich@gmail.com>
2024-08-15 18:07:42 +0300

    Added a bottom line for the header

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -11,7 +11,7 @@ interface Props {
 
 export const Header: React.FC<Props> = ({ className }) => {
   return (
-    <header className={cn("", className)}>
+    <header className={cn("border border-b", className)}>
       <Container className="flex items-center justify-between py-8">
         <div className="flex items-center gap-1">
           <Image src="/icons/logo.svg" alt="Logo" width={40} height={40} />
